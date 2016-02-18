70cb4b1f654dfaa668bc7526e391a21f1b6acbda
mcibique <mcibique@hotmail.com>
2016-02-18 19:01:08 +0000

    Added CSRF tests.

---
+++ tests/e2e/csrf-spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let request = require('request');
+
+const baseUrl = 'https://localhost:5000';
+const loginUrl = `${baseUrl}/login/`;
+
+describe('csrf', () => {
+  it ('should return back to login when token is not present', (cb) => {
+      request.post({
+        url: loginUrl,
+        form: {
+          username: 'a',
+          password: 'b'
+        }
+      }, (error, response) => {
+        if (error) {
+          return cb(error);
+        }
+        expect(response.statusCode).toBe(303);
+        expect(response.headers['location']).toBe('/login/');
+        cb();
+      });
+  });
+
+  it ('should return back to login when token is wrong', (cb) => {
+      request.post({
+        url: loginUrl,
+        form: {
+          username: 'a',
+          password: 'b',
+          _csrf: 'random invalid token'
+        }
+      }, (error, response) => {
+        if (error) {
+          return cb(error);
+        }
+        expect(response.statusCode).toBe(303);
+        expect(response.headers['location']).toBe('/login/');
+        cb();
+      });
+  });
+});
