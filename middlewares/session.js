d774328cf968bf1213e92c5ada8e3a2b3e8aae5b
mcibique <mcibique@hotmail.com>
2016-02-20 11:55:07 +0000

    Added redis for storing sessions.

--- middlewares/session.js
+++ middlewares/session.js
@@ -1,11 +1,23 @@
 'use strict';
 
 let session = require('express-session');
+let RedisStore = require('connect-redis')(session);
 let ms = require('ms');
 
 const config = require('../helpers/config');
+const isDebug = require('../helpers/debug');
+
+let store;
+if (config.session.redis) {
+  store = new RedisStore(config.session.redis);
+} else if (isDebug) {
+  store = new session.MemoryStore();
+} else {
+  throw 'Unable to configure session store.';
+}
 
 module.exports = session({
+  store: store,
   name: config.session.cookieName,
   resave: false,
   rolling: true,
